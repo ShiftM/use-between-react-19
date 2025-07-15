2d31dfef1a3f7825c09feb8d683f877bf0c084f6
Luis Ramirez <emmanuelluis.ramirez@gmail.com>
2025-07-16 06:47:01 +0800

    Update react-shared-internals.ts

--- src/lib/react-shared-internals.ts
+++ src/lib/react-shared-internals.ts
@@ -12,4 +12,4 @@ type ReactSharedInternalsType = {
 export const ReactSharedInternals =
   (React as any).__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED as ReactSharedInternalsType
 
-export const ReactCurrentDispatcher = ReactSharedInternals.ReactCurrentDispatcher
+export const ReactCurrentDispatcher = ReactSharedInternals.H
